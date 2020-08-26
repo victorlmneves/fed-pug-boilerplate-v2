4d9115545b69dbfd0a2619c16cfdf8d6a658634a
Victor Neves <info@vitorneves.com>
2020-08-26 19:44:21 +0100

    Added the initial dynamic content

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,47 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const fetch = require('node-fetch');
 
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Fed Pug Boilerplate with Express" });
-});
+const requestOptions = {
+  method: 'GET',
+  redirect: 'follow'
+}
 
-module.exports = router;
+let heroSection = null
+let appsSection = null;
+
+const heroSectionContent = fetch('https://express-api.victorneves.dev/hero_section/read.php', requestOptions)
+  .then((resp) => resp.json())
+  .then((res) => {
+    return res
+  })
+  .catch((error) => console.log(JSON.stringify(error)));
+
+  heroSectionContent.then(function (res) {
+    heroSection = res.records[0];
+  });
+
+const appsSectionContent = fetch('https://express-api.victorneves.dev/apps_section/read.php', requestOptions)
+  .then((resp) => resp.json())
+  .then((res) => {
+    const obj = {
+      title: res.records[0].title,
+      text: {
+        __class: res.records[0].text_css,
+        text: res.records[0].text,
+      },
+    }
+
+    return obj
+  })
+  .catch((error) => console.log(JSON.stringify(error)));
+
+  appsSectionContent.then(function (res) {
+    appsSection = res;
+  });
+
+  router.get("/", function (req, res, next) {
+    res.render("index", { heroSection, appsSection });
+  })
+
+module.exports = router
